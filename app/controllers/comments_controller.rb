aa589f5ae8f7e1a0a7a1bcde0d21d3034281c1bb
Guilherme Tupinambá Durães <40327476+guilhermetupi@users.noreply.github.com>
2021-03-16 01:23:05 -0300

    Add pages to app

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
-  before_action :find_post, only: [:create]
+  before_action :find_post, only: [:create, :destroy]
 
   def create
     @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
     redirect_to post_path(@post)
   end
 
